3cd1bbb5552acf7eae9f79a5419037ef710b217c
Pascal-Emmanuel Lachance <17782420+Raesangur@users.noreply.github.com>
2023-01-28 17:17:34 -0500

    Update cat-factory.cpp

--- cat-factory.cpp
+++ cat-factory.cpp
@@ -2,11 +2,14 @@
 #include <string>
 
 int main() {
+	// Get prompt from console
 	std::string line;
 	std::cin >> line;
 
+	// line for testing
 	//line = "reqcahreetqttrcrtecatreqCAhreeTqTTrCrTeCAT";
 
+	// remove all non-"c|a|t" characters
 	std::string newline;
 	for (char c : line)
 	{
@@ -14,6 +17,7 @@ int main() {
 			newline += c;
 	}
 
+	// Count number of occurences of the word "cat"
 	int occurences = 0;
 	std::string::size_type it = 0;
 	while ((it = newline.find("cat", it)) != std::string::npos)
