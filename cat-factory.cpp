ffb9299e74da77ac5450e46d5132d0939e2759b3
Pascal-Emmanuel Lachance <17782420+Raesangur@users.noreply.github.com>
2023-01-28 20:24:00 -0500

    Update cat-factory.cpp

--- cat-factory.cpp
+++ cat-factory.cpp
@@ -2,29 +2,29 @@
 #include <string>
 
 int main() {
-	// Get prompt from console
-	std::string line;
-	std::cin >> line;
+    // Get prompt from console
+    std::string line;
+    std::cin >> line;
 
-	// line for testing
-	//line = "reqcahreetqttrcrtecatreqCAhreeTqTTrCrTeCAT";
+    // line for testing
+    //line = "reqcahreetqttrcrtecatreqCAhreeTqTTrCrTeCAT";
 
-	// remove all non-"c|a|t" characters
-	std::string newline;
-	for (char c : line)
-	{
-		if (c == 'c' || c == 'a' || c == 't')
-			newline += c;
-	}
+    // remove all non-"c|a|t" characters
+    std::string newline;
+    for (char c : line)
+    {
+        if (c == 'c' || c == 'a' || c == 't')
+            newline += c;
+    }
 
-	// Count number of occurences of the word "cat"
-	int occurences = 0;
-	std::string::size_type it = 0;
-	while ((it = newline.find("cat", it)) != std::string::npos)
-	{
-		it += 3;
-		occurences++;
-	}
+    // Count number of occurences of the word "cat"
+    int occurences = 0;
+    std::string::size_type it = 0;
+    while ((it = newline.find("cat", it)) != std::string::npos)
+    {
+        it += 3;
+        occurences++;
+    }
 
-	std::cout << occurences;
+    std::cout << occurences;
 }
